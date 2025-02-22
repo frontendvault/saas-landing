b3d472c2e41bd56904776493676565f128551bc0
manoj pathania <manoj07.pathania@gmail.com>
2025-02-22 20:28:25 +0530

    smooth scroll

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FFFEF0] `}
       >
